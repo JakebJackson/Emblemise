62001f3ff4e0bc27029d00d90fee4b09421220fa
Jake Jackson <142851548+JakebJackson@users.noreply.github.com>
2024-02-12 21:34:17 +1100

    Changed the render class function into a class constructor, added basic testing functionality

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Shapes = require('./lib/shapes');
 
 function handleCreateLogo(text, textColour, shape, shapeColour) {
-    const userShape = handleCreateShape(shape, shapeColour)
+    const userShape = Shapes.render(shape, shapeColour);
     const userText = `<text x="50%" y="52%" dominant-baseline="middle" text-anchor="middle" fill="${textColour}" font-size="36">${text}</text>`;
     return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">${userShape}${userText}</svg>`;
-}
-
-function handleCreateShape(shape, shapeColour) {
-    switch (shape) {
-        case 'Square':
-            return `<rect x="100" y="50" width="100" height="100" fill="${shapeColour}"/>`;
-        case 'Circle': 
-            return `<circle cx="150" cy="100" r="50" fill="${shapeColour}"/>`;
-        case 'Triangle':
-            return `<polygon points="150,25 250,150 50,150" fill="${shapeColour}"/>`;
-    }
 }
 
 inquirer.prompt([
